4fb70a1c39523ca0bbcd060406d57cc293670032
Tiago Gomes <tidgomes@gmail.com>
2024-01-04 12:52:14 -0300

    design the header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  styles: ['h1 {color: red;}'],
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
