b7b15f42c33d5f2d761a91276b0020fe5e1a1a09
Tiago Gomes <tidgomes@gmail.com>
2024-01-11 09:37:11 -0300

    loop through shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-  public ingredients: []
+  public ingredients: Ingredient[] = [
+    new Ingredient('Feijão', 300),
+    new Ingredient('Orelha de Porco', 8)
+  ]
 }
