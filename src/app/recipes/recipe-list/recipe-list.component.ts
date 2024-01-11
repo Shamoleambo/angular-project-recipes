a1154fd71bd411c6503d52480eb2a4bbf45f457e
Tiago Gomes <tidgomes@gmail.com>
2024-01-11 09:20:01 -0300

    implement recipe-detail component view

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe("feijoada", "prato com feijão e porco", "https://assets.unileversolutions.com/recipes-v2/229468.jpg")
+    new Recipe("feijoada", "prato com feijão e porco", "https://assets.unileversolutions.com/recipes-v2/229468.jpg"),
+    new Recipe("Macarronada", "prato com bastante macarrão", "https://www.mariareceita.com.br/wp-content/uploads/2022/11/receita-macarronada-simples-facil-com-molho.jpg")
   ]
 }
